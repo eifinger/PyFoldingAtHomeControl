48e86db126c2bfaf5e194e145d2d2b5d04d9a544
Kevin Eifinger <k.eifinger@googlemail.com>
2020-03-26 18:27:22 +0100

    add setuptools

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+long_description = open('README.md').read()
+
+setup(
+    name='PyFoldingAtHomeControl',
+    version='00.1',
+    license='MIT',
+    url='https://github.com/eifinger/PyFoldingAtHomeControl',
+    author='Kevin Eifinger',
+    author_email='k.eifinger@googlemail.com',
+    description='Python lib to get stats from your Folding@Home clients.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=[],
+    zip_safe=True,
+    platforms='any',
+    install_requires=list(val.strip() for val in open('requirements.txt')),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        "License :: OSI Approved :: MIT License"
+    ]
+)
