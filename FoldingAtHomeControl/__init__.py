e900b797948b857aef62fbf0c43e76f30f4d48a8
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2022-02-08 18:13:43 +0100

    fix: Expose PowerLevel

--- FoldingAtHomeControl/__init__.py
+++ FoldingAtHomeControl/__init__.py
@@ -1,5 +1,6 @@
 """Define module-level imports."""
 # pylint: disable=C0103
+from .const import PowerLevel  # noqa
 from .const import PyOnMessageTypes  # noqa
 from .exceptions import (  # noqa
     FoldingAtHomeControlAuthenticationFailed,
